925b56220d681cb86522a05912fdb6a6bbbead26
Xuanzhe XIA <xuanzhe.xia@etu.uca.fr>
2024-02-04 15:45:10 +0100

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
+import "./App.css"
+import Navigation from "./components/Navigation";
+
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="brand_logo" />
-        </div>
-        
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>login</button>
-      </nav>
+      <Navigation />
     </div>
   );
 };
